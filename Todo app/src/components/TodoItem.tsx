09528585ede26bb781bdad0ca63e37748ebe3b8c
Mark Mozer <Mark@Tamass-MacBook-Air.local>
2025-09-26 09:42:00 +0200

    delete task and checkbox change to button

--- Todo app/src/components/TodoItem.tsx
+++ Todo app/src/components/TodoItem.tsx
@@ -1,28 +1,29 @@
-import type { Task } from "../models/task";
+import type { Task } from '../models/task';
 
 const TodoItem = ({
-  task,
-  index,
-  toggleCompleted,
+	task,
+	index,
+	toggleCompleted,
+	deleteTask,
 }: {
-  task: Task;
-  index: number;
-  toggleCompleted: (index: number) => void;
+	task: Task;
+	index: number;
+	toggleCompleted: (index: number) => void;
+	deleteTask: (index: number) => void;
 }) => {
-  return (
-    <>
-      <li>
-        <label>
-          <p>{task.text}</p>
-          <input
-            type="checkbox"
-            checked={task.completed}
-            onChange={() => toggleCompleted(index)}
-          />
-        </label>
-      </li>
-    </>
-  );
+	return (
+		<>
+			<li>
+				<label>
+					<p>{task.text}</p>
+					<button onClick={() => toggleCompleted(index)}>
+						{task.completed ? 'Kész' : 'Nincs Kész'}
+					</button>
+					<button onClick={() => deleteTask(index)}>Törlés</button>
+				</label>
+			</li>
+		</>
+	);
 };
 
 export default TodoItem;
--- Todo app/src/components/TodoList.tsx
+++ Todo app/src/components/TodoList.tsx
@@ -1,54 +1,63 @@
-import { useState } from "react";
-import TodoButton from "./TodoButton";
-import TodoItem from "./TodoItem";
-import type { Task } from "../models/task";
-import TodoCounter from "./TodoCounter";
-import TodoForm from "./TodoForm";
+import { useState } from 'react';
+import TodoButton from './TodoButton';
+import TodoItem from './TodoItem';
+import type { Task } from '../models/task';
+import TodoCounter from './TodoCounter';
+import TodoForm from './TodoForm';
 
 const Todolist = () => {
-  const [tasks, setTasks] = useState<Task[]>([
-    { text: "első feladatom", completed: false },
-    { text: "másdik feladatom", completed: false },
-    { text: "harmadik feladatom", completed: false },
-    { text: "negyedik feladatom", completed: false },
-  ]);
+	const [tasks, setTasks] = useState<Task[]>([
+		{ text: 'első feladatom', completed: false },
+		{ text: 'másdik feladatom', completed: false },
+		{ text: 'harmadik feladatom', completed: false },
+		{ text: 'negyedik feladatom', completed: false },
+	]);
 
-  const [isModalOpen, setModalOpen] = useState(false);
+	const [isModalOpen, setModalOpen] = useState(false);
 
-  const completedCount = tasks.filter((task) => task.completed).length;
-  const notCompletedCount = tasks.length - completedCount;
+	const completedCount = tasks.filter((task) => task.completed).length;
+	const notCompletedCount = tasks.length - completedCount;
 
-  const toggleCompleted = (index: number) => {
-    const newTasks = [...tasks];
-    newTasks[index].completed = !newTasks[index].completed;
-    setTasks(newTasks);
-  };
-  const addTask = (item: Task) => {
-    const newTasks = [...tasks];
-    newTasks.push(item);
-    setTasks(newTasks);
-  };
+	const toggleCompleted = (index: number) => {
+		const newTasks = [...tasks];
+		newTasks[index].completed = !newTasks[index].completed;
+		setTasks(newTasks);
+	};
+	const addTask = (item: Task) => {
+		const newTasks = [...tasks];
+		newTasks.push(item);
+		setTasks(newTasks);
+	};
+	const deleteTask = (index: number) => {
+		const newTasks = [...tasks];
+		newTasks.splice(index, 1);
+		setTasks(newTasks);
+	};
 
-  return (
-    <div className="parent-container">
-      <h1>Todo list</h1>
-      <ul id="todo-list">
-        {tasks.map((item, index) => (
-          <TodoItem
-            key={index}
-            index={index}
-            task={item}
-            toggleCompleted={toggleCompleted}
-          />
-        ))}
-      </ul>
-      <TodoCounter todo={notCompletedCount} done={completedCount} />
-      <TodoButton setModalOpen={setModalOpen} />
-      {isModalOpen && (
-        <TodoForm onClose={() => setModalOpen(false)} addTask={addTask} />
-      )}
-    </div>
-  );
+	return (
+		<div className="parent-container">
+			<h1>Todo list</h1>
+			<ul id="todo-list">
+				{tasks.map((item, index) => (
+					<TodoItem
+						key={index}
+						index={index}
+						task={item}
+						toggleCompleted={toggleCompleted}
+						deleteTask={deleteTask}
+					/>
+				))}
+			</ul>
+			<TodoCounter todo={notCompletedCount} done={completedCount} />
+			<TodoButton setModalOpen={setModalOpen} />
+			{isModalOpen && (
+				<TodoForm
+					onClose={() => setModalOpen(false)}
+					addTask={addTask}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default Todolist;
